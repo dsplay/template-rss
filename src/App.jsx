c06e7571863fbd80df1a77eb8202556246c7bcdb
RenanMacielLuna <renanluna09@gmail.com>
2020-03-14 08:38:56 -0300

    Envio do template

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import MyQrCode from './components/my_qrcode/my_qrcode.component';
+import NewsItem from './components/news_item/news_item';
+import Loading from './components/loading/loading';
 import {
-    // values
-    media, // current media
-    config, // player configuration
-    template, // custom template values
-    // utility functions
-    tval, // custom template string value
-    tbval, // custom template boolean value
-    tival, // custom template int value
-    tfval, // custom template float value
-    isVertical, // boolean flag to indicate screen orientation
-
+  // values
+  media, // current media
+  config // player configuration
+  //   template, // custom template values
+  //   // utility functions
+  //   tval, // custom template string value
+  //   tbval, // custom template boolean value
+  //   tival, // custom template int value
+  //   tfval // custom template float value
+  //   //isVertical // boolean flag to indicate screen orientation
 } from '@dsplay/template-utils';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const { duration } = media;
 const { orientation, locale } = config;
 
-function App() {
+class App extends Component {
+  render() {
     return (
-        <div className="App">
-            <h1>DSPLAY Template</h1>
-            <h2>Raw Values</h2>
-            <div>
-                <p>Config:</p>
-                <pre>{JSON.stringify(config, null, 4)}</pre>
-
-                <p>Media:</p>
-                <pre>{JSON.stringify(media, null, 4)}</pre>
-
-                <p>Template:</p>
-                <pre>{JSON.stringify(template, null, 4)}</pre>
-            </div>
-
-            <h2>Configuration Values Examples</h2>
-            <div>
-                <p>
-                    Orientation:
-                    <span className="val">{orientation}</span>
-                </p>
-                <p>
-                    Locale:
-                    <span className="val">{locale}</span>
-                </p>
-            </div>
-            <h2>Media Values Examples</h2>
-            <div>
-                <p>
-                    Duration:
-                    <span className="val">{duration}</span>
-                </p>
-            </div>
-            <h2>Custom Template Var Examples</h2>
-            <div>
-                <p>
-                    String:
-                    <span className="val">{tval('title', 'Default Value')}</span>
-                </p>
-                <p>
-                    Boolean:
-                    <span className="val">{tbval('expanded', true) ? 'Yes' : 'No'}</span>
-                </p>
-                <p>
-                    Int:
-                    <span className="val">{tival('page_size', 10)}</span>
-                </p>
-                <p>
-                    Double:
-                    <span className="val">{tfval('rate', .75)}</span>
-                </p>
-                <p>
-                    Image:
-                    <img className="val" src={tval('logo')} />
-                </p>
-                <p>
-                    Vertical?:
-                    <span className="val">{isVertical ? 'Yes' : 'No'}</span>
-                </p>
-            </div>
-        </div>
+      <div className='App' id='root'>
+        {<NewsItem />}
+      </div>
     );
+  }
 }
-
 
 export default App;
---
+++ src/components/my_qrcode/my_qrcode.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { media } from '@dsplay/template-utils';
+
+function MyQrCode() {
+  return <img src={media.qrCode} alt='Logo' />;
+}
+
+export default MyQrCode;
